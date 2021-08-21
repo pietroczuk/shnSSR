2acfa324e7bcae2b433cc1a92efbb6620ea01662
andrzej pietroczuk <andrzej@pietroczuk.pl>
2021-08-21 21:17:32 +0200

    move context logic to context component provider

--- src/client/components/contentCointainer/ContentCointainer.js
+++ src/client/components/contentCointainer/ContentCointainer.js
@@ -1,21 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './contentCointainer.modules.scss';
 import withStyles from 'isomorphic-style-loader/withStyles';
 
-import ContenerContext from './contenerContext/contenerContext';
+import { ContenerContextProvider } from './contenerContext/contenerContext';
 
 const ContentCointainer = props => {
     const { miltirow } = props;
-    const [mainRef, setMainRef] = useState(null);
-    const setMainRefHandler = reference => {
-        setMainRef(reference);
-    }
     return (
-        <div className={`${styles.page_container} ${miltirow ? styles.multirow : ''}`}>
-            <ContenerContext.Provider value={{ mainContentRef: mainRef, setMainContentRef: setMainRefHandler }}>
+        <ContenerContextProvider>
+            <div className={`${styles.page_container} ${miltirow ? styles.multirow : ''}`}>
                 {props.children}
-            </ContenerContext.Provider>
-        </div>
+            </div>
+        </ContenerContextProvider>
     )
 }
 export default withStyles(styles)(ContentCointainer);
--- src/client/components/contentCointainer/contenerContext/contenerContext.js
+++ src/client/components/contentCointainer/contenerContext/contenerContext.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ContenerContext = React.createContext({ mainContentRef: null, setMainContentRef: () => { } });
-
 export default ContenerContext;
+
+export const ContenerContextProvider = (props) => {
+    const [mainRef, setMainRef] = useState(null);
+    const setMainRefHandler = reference => {
+        setMainRef(reference);
+    }
+    return <ContenerContext.Provider value={{mainContentRef: mainRef, setMainContentRef: setMainRefHandler}}>{props.children}</ContenerContext.Provider>
+}
