69822fcb0ddcad91d130fa33a40a6ea5c74a67e2
andrzej pietroczuk <andrzej@pietroczuk.pl>
2021-11-28 10:13:27 +0100

    fixed css fixedbar and svg aspect ratio

--- src/client/components/svg/blank/Blank.js
+++ src/client/components/svg/blank/Blank.js
@@ -2,7 +2,7 @@ import React from "react";
 const Blank = ({ width, height }) => {
     const viewBoxWidth = width ? width : 300;
     const viewBoxHeight = height ? height : 300;
-
-    return <svg viewBox={"0 0 "+viewBoxWidth+" "+viewBoxHeight}></svg>;
+ 
+    return <svg style={{width: '100%', height: '100%'}} viewBox={"0 0 "+viewBoxWidth+" "+viewBoxHeight}></svg>;
 }
 export default Blank;
