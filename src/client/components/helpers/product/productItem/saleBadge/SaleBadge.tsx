f633dc21b91c443827607185ec816dbf6c14f370
andrzej pietroczuk <andrzej@pietroczuk.pl>
2022-04-14 15:20:42 +0200

    changing useselector in badge for better performance

--- src/client/components/helpers/product/productItem/saleBadge/SaleBadge.tsx
+++ src/client/components/helpers/product/productItem/saleBadge/SaleBadge.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from './saleBadge.scss';
 import withStyles from "isomorphic-style-loader/withStyles";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../../client";
 
 interface SaleBadgeProps {
     sale: {
@@ -13,10 +15,27 @@ interface SaleBadgeProps {
 
 const SaleBadge: React.FC<SaleBadgeProps> = props => {
     const { sale } = props;
-    return (
-        <div className={styles.badge}>
-            -{sale.percent}%
-        </div>
+    if (!sale || (sale && !sale.enable)) {
+        return <React.Fragment />
+    }
+
+    const showBadge = useSelector((state: RootState) => {
+        const date = state.User.today.date
+        if (date >= sale.startSale) {
+            return true;
+        }
+        return false;
+    }
     );
+
+    if (showBadge) {
+        return (
+            <div className={styles.badge}>
+                -{sale.percent}%
+            </div>
+        );
+    }
+    return <React.Fragment />
+
 }
 export default withStyles(styles)(SaleBadge);
