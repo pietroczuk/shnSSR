6e8e02c55b7a427a3222c7a4f0b51d777d4faeb3
andrzej pietroczuk <andrzej@pietroczuk.pl>
2022-06-21 00:01:31 +0200

    changes test script in package.json and add c8

--- src/client/utils/utilsFrondend.test.ts
+++ src/client/utils/utilsFrondend.test.ts
@@ -2,6 +2,15 @@ import { it, expect } from 'vitest';
 import { getPageTypeAsString } from './utilsFrondend';
 
 it('it should return pageType as string', () => {
-    const result = getPageTypeAsString('productPage');
-    expect(result).toBe('product');
+    const inputString = 'productPage';
+    const expectedResult = 'product';
+
+    const result = getPageTypeAsString(inputString);
+    expect(result).toBe(expectedResult);
+});
+it('it should return wrong pageType as undefined', () => {
+    const inputString = 'productPage2';
+
+    const result = getPageTypeAsString(inputString);
+    expect(result).toBeUndefined;
 });
