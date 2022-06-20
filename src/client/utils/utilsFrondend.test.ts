c01752f2b807a6c7f0586a13a40169667d692033
andrzej pietroczuk <andrzej@pietroczuk.pl>
2022-06-21 00:40:17 +0200

    add test suits

--- src/client/utils/utilsFrondend.test.ts
+++ src/client/utils/utilsFrondend.test.ts
@@ -1,28 +1,28 @@
-import { it, expect } from 'vitest';
+import { it, expect, describe } from 'vitest';
 import { getPageTypeAsString } from './utilsFrondend';
 
-it('it should return correct pageType as string', () => {
-    const inputString = 'productPage';
-    const expectedResult = 'product';
-
-    const result = getPageTypeAsString(inputString);
-    expect(result).toBe(expectedResult);
-});
-it('it should return wrong pageType as undefined', () => {
-    const inputString = 'productPage2';
-
-    const result = getPageTypeAsString(inputString);
-    expect(result).toBeUndefined;
-});
-it('it should return empty pageType as undefined', () => {
-    const inputString = '';
-
-    const result = getPageTypeAsString(inputString);
-    expect(result).toBeUndefined;
-});
-it('it should throw a error if no value is passed into the function', () => {
-    const resultFn = () => {
-        getPageTypeAsString();
-    }
-    expect(resultFn).toThrow;
-});
+describe('getPageTypeAsString()', ()=> {
+    it('it should return correct pageType as string', () => {
+        const inputString = 'productPage';
+        const expectedResult = 'product';
+    
+        const result = getPageTypeAsString(inputString);
+        expect(result).toBe(expectedResult);
+    });
+    it('it should return wrong pageType as undefined', () => {
+        const inputString = 'productPage2';
+        const inputString2 = '';
+    
+        const result = getPageTypeAsString(inputString);
+        const result2 = getPageTypeAsString(inputString2);
+        expect(result).toBeUndefined();
+        expect(result2).toBeUndefined();
+    });
+    
+    // it('it should not throw a error if no value is passed into the function', () => {
+    //     const resultFn = () => {
+    //         getPageTypeAsString();
+    //     }
+    //     expect(resultFn).not.toThrow();
+    // });
+})
